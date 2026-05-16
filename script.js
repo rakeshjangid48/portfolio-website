abb64aaf34fe1899341da8b903d3bda27a353581
Rakesh Jangid <rakeshjangid66644@gmail.com>
2026-05-17 00:15:37 +0530

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const text = [
+  "Aspiring Software Developer",
+  "Frontend Developer",
+  "Web Designer"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+  if(count === text.length){
+    count = 0;
+  }
+
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".hero-text h3").textContent = letter;
+
+  if(letter.length === currentText.length){
+    count++;
+    index = 0;
+    setTimeout(type,1500);
+  }
+  else{
+    setTimeout(type,100);
+  }
+
+})();
